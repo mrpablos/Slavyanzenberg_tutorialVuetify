a65ffabddcdf2785e83f5e2cd6bb98756899e636
mrpablos <141761312+mrpablos@users.noreply.github.com>
2023-09-14 22:56:34 +0300

    Slavyanzenberg_tutorialVuetify
    Slavyanzenberg_tutorialVuetify

---
+++ vuetify_pasha/src/layouts/default/Default.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+   <v-tabs
+      v-model="tab"
+      bg-color="primary"
+    >
+      <v-tab value="one">Item One</v-tab>
+      <v-tab value="two">Item Two</v-tab>
+      <v-tab value="three">Item Three</v-tab>
+    </v-tabs>
+
+<v-layout>
+      <v-navigation-drawer
+        v-model="drawer"
+        :rail="rail"
+        permanent
+        @click="rail = false"
+      >
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+          title="John Leider"
+          nav
+        >
+          <template v-slot:append>
+            <v-btn
+              variant="text"
+              icon="mdi-chevron-left"
+              @click.stop="rail = !rail"
+            ></v-btn>
+          </template>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-home-city" title="Home" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-account" title="My Account" value="account"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-group-outline" title="Users" value="users"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main style="height: 250px"></v-main>
+    </v-layout>
+
+    <div><Home /></div>
+    
+
+    <v-footer class="bg-grey-lighten-1" height="50" app absolute>
+    <v-row justify="center" no-gutters>
+      <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+  </v-app>
+</template>
+
+<script setup>
+  import Home from '@/views/Home.vue'
+</script>
